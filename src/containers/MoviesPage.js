e2d2fe9d0880a1d8164fc5d08bc69078d3b77813
briecoyle <brie.coyle@gmail.com>
2017-07-01 16:03:25 -0400

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList';
+import MoviesShow from './MoviesShow'
 
-const MoviesPage = ({ movies }) => 
+const MoviesPage = ({ movies }) =>
   <div>
     <MoviesList movies={movies} />
+    <Route path={`${match.url}/:movieId`} component={MoviesShow} />
+    <Rout exact path={match.url} render={() => (
+      <h3>Please select a Movie from the list.</h3>
+    )} />
   </div>;
 
 const mapStateToProps = (state) => {
